a32b9f2a0c7f8ddc0f538d8d3cfdfd7ec76b4bd0
Muhammad faraz <farazmohammad190@gmail.com>
2024-04-03 20:46:40 +0500

    complete login signup api

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+const dotenv = require('dotenv');
+const router = require('../routes/index.js');
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8080;  
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(router);
+
+mongoose.connect(process.env.DB_URI).then((res)=>{
+    console.log("database connected..")
+}).catch((err)=>{
+    console.log(err)
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on http:localhost:${PORT}`)
+})
